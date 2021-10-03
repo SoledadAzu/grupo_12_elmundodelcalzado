cc59f246973507b8f966993dac169011ec479d24
Alexander Ruben Mamani <labm2014@gmail.com>
2021-10-03 20:49:30 -0300

    agregando rutas

--- site/src/routes/admin.js
+++ site/src/routes/admin.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const {admin,create,edit,del} = require('../controllers/admin')
 
 router.get('/',admin)
-router.get('/create',create)
 router.get('/edit',edit)
 router.get('/eliminar',del)
-
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create/',create); 
+router.post('/',store); 
 
 module.exports = router
