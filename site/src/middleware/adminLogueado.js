5a5a01764ca220d620741a9e3b48947c5c9e7558
MierezAugusto <augustomierez@gmail.com>
2021-10-24 22:29:58 -0300

    modificaciones en usuarios registrados y header

--- site/src/middleware/adminLogueado.js
+++ site/src/middleware/adminLogueado.js
@@ -2,11 +2,11 @@
 function adminLog(req,res,next){
     if(req.cookies.recordame!== undefined&& req.cookies.recordame.rol === "admin"){
        
-        res.locals.userAdmin = req.cookies.recordame.email
+        res.locals.userAdmin = req.cookies.recordame.nombre
             
     }else if(req.session.usuarioLogueado!== undefined&& req.session.usuarioLogueado.rol === "admin"){
        
-        res.locals.userAdmin = req.session.usuarioLogueado.email
+        res.locals.userAdmin = req.session.usuarioLogueado.nombre
             
     }
     next()
