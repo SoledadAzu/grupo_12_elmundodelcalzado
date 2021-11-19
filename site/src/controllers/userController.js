352937cf3c000d78b0ac89666f5e4838eefae408
sandra cruz <sandcz2021@gmail.com>
2021-11-19 19:28:07 -0300

    subo cambios

--- site/src/controllers/userController.js
+++ site/src/controllers/userController.js
@@ -10,3 +10,18 @@ const controller ={
     
 }
 module.exports=controller
+
+db.user.create({
+    user: user,
+    email: email,
+    password: bcrypt.hashSync(password, 7),
+    rolesId: 1,
+    avatar: req.file ? req.file.filename: "defaultAvatarImage.png"
+})
+.then(() => {
+    res.redirect("/login")
+})
+    
+
+res.render("user/register")
+
