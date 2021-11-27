5eeae060185fe37bc4a505be42b6a8382a69ec7d
MierezAugusto <augustomierez@gmail.com>
2021-11-27 10:29:36 -0300

    cambio en tabla  colores

--- site/src/database/script/data.sql
+++ site/src/database/script/data.sql
@@ -80,6 +80,7 @@ DROP TABLE IF EXISTS `colores`;
 CREATE TABLE `colores` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
+  `id_productos` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
