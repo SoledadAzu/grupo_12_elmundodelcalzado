3fe97d11011b5f6abacf72128e0d62a8ddb1b9c0
MierezAugusto <augustomierez@gmail.com>
2021-11-27 11:44:37 -0300

    cambios en talles

--- site/src/database/models/talles.js
+++ site/src/database/models/talles.js
@@ -10,16 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-<<<<<<< HEAD
-      Talles.hasMany(models.Productos, { 
-        as: "Productos",
-        foreignKey: 'id_talles',    
-=======
      
     Talles.belongsTo(models.Productos, {
       as: "Producto",
       foreignKey: "id_producto",
->>>>>>> 1dba871f426f24ecf0b8412bb117f740dc9c4fb2
     })
   }
   };
