7775928692a4176041b2869956e695e0489fa4bf
MierezAugusto <augustomierez@gmail.com>
2021-12-13 10:51:13 -0300

    validacion front

--- site/public/js/createvalidafront.js
+++ site/public/js/createvalidafront.js
@@ -1,29 +1,84 @@
-let form = document.querySelector("form");
-let submitbutton = document.querySelector("btn");
+
+let form = document.getElementById("formcreate");
+let submitbutton = document.getElementById("submit-btn");
 
 let timeout = null
 
-document.querySelectorAll(".labelForm").forEach((divInput) => {
-    const boxInput = divInput.querySelector("input");
+let errors = {
+  title: true,
+  price: true,
+  colors: true,
+  detalles: true,
+};
 
-    boxInput.addEventListener("keydown",(e)=> {
+document.querySelectorAll("#inputbox").forEach((box) => {
+    const boxInput = box.querySelector("input");
+
+    boxInput.addEventListener("keydown", (event) => {
         clearTimeout(timeout);
         timeout = setTimeout(() => {
-            console.log(`Input ${boxInput.name} value:`, boxInput.value);
+             console.log(`Input ${boxInput.name} value:`, boxInput.value); 
 
-            validation(box)
-        }, 300)
+            validation(box,boxInput)
+        }, 300);
     });
 });
 
-validation = (box, boxInput) => {
-    if (boxInput.value == '') {
-      showError(true, box, boxInput);
+
+validation = (box,boxInput) => {
+  if (boxInput.name == 'title') {  
+    if (boxInput.value ==='') {
+      showError(true,box,boxInput);
     } else {
-      showError(false, box, boxInput);
+      showError(false,box,boxInput);
     }
-};
-showError = (check, box, boxInput) => {
+  }
+  if (boxInput.name == 'colors') {  
+    if (boxInput.value ==='') {
+      showError(true,box,boxInput);
+    } else {
+      showError(false,box,boxInput);
+    }
+  }
+  if (boxInput.name == 'detalles') {  
+    if (boxInput.value ==='') {
+      showError(true,box,boxInput);
+    } else {
+      showError(false,box,boxInput);
+    }
+  }
+    if (boxInput.name =='price') {
+      if (boxInput.value ==='') {
+        showError(true,box,boxInput);
+      } else {
+        showError(false,box,boxInput);
+      }
+            
+      if (boxInput.value.length <= 3) {
+        showError(true,box,boxInput);
+      } else {
+        showError(false,box,boxInput);
+      }
+    }
+  
+
+    submitActivado();
+  };
+let checkboxresul=document.querySelectorAll(".checkdiv").forEach(() => {
+submitbutton.addEventListener("clic", (event)=>{  
+      var elementosTotales = checkdiv.elements.length;
+      var total_checked = 0;
+      for(i=0;i<elementosTotales;i++)
+         {
+     if((checkdiv.elements[i].type=="checkbox")
+               &&(checkdiv.elements[i].checked))
+               { total_checked++; }
+         }
+      alert("hay "+total_checked+" checkboxs marcados")
+        })
+});
+console.log(checkboxresul)
+showError = (check,box,boxInput) => {
     if (check) {
       box.classList.remove('form-success');
       box.classList.add('form-error');
@@ -34,3 +89,20 @@ showError = (check, box, boxInput) => {
       errors[boxInput.name] = false;
     }
 };
+submitActivado = () => {
+  if (errors.title || errors.price || errors.colors || errors.detalles ){
+    submitbutton.toggleAttribute('disabled', true);
+  } else {
+    submitbutton.toggleAttribute('disabled', false);
+  }
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  console.log([...formData]);
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
+});
+
