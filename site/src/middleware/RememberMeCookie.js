4176b81786529c9efd716047caede5410f777d22
Alexander Ruben Mamani <labm2014@gmail.com>
2021-12-19 14:52:30 -0300

    arreglando cierre de session y cookies

--- site/src/middleware/RememberMeCookie.js
+++ site/src/middleware/RememberMeCookie.js
@@ -1,6 +1,6 @@
 function rememberMe(req,res,next){
     
-    if(req.cookies.rememberMe){
+    if(req.cookies.rememberMe != undefined && req.session.usuarioLogueado == undefined){
         req.session.usuarioLogueado = req.cookies.rememberMe
     }
     next()
--- site/src/middleware/permisoMiddleware.js
+++ site/src/middleware/permisoMiddleware.js
@@ -1,8 +1,10 @@
 function permiso(req,res,next){
     if(res.locals.user.rol==="admin"){
+        console.log(res.locals.user.rol)
         next()
     }else{
         res.render("extras/denegado")
+        
     }
       
 }
