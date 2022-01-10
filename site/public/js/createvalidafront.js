4653bd1a80dac9e7a54b49401154467541e4a9c3
MierezAugusto <augustomierez@gmail.com>
2022-01-09 22:08:28 -0300

    validaciones front

--- site/public/js/createvalidafront.js
+++ site/public/js/createvalidafront.js
@@ -11,7 +11,7 @@ const smalldetalles = document.querySelector(".error-detalle")
 const smalldescription = document.querySelector(".error-description")
 const smalltalles = document.querySelector(".errortalles")
 let errors = {
-title: true,
+  title: true,
   price: true,
   colors1: true,
   colors2: true,
@@ -20,7 +20,11 @@ title: true,
   detalles2: true,
   detalles3: true,
   detalles4: true,
-  description: true
+  description: true,
+  marca: true,
+  genero: true,
+  temporada: true,
+  outlet: true
 };
 const expresiones = {
   precio: /^\d{4,6}$/
@@ -34,26 +38,26 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         title.classList.remove('form-success');
         title.classList.add('form-error');
-        smalltitle.innerHTML= "* Debe completar el campo "
+        smalltitle.innerHTML = "* Debe completar el campo "
         errors[e.target.id] = true;
-        
+
       } else {
         title.classList.remove('form-error');
         title.classList.add('form-success');
-        smalltitle.innerHTML= ""
+        smalltitle.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
     case "price":
-      if (expresiones.precio.test(e.target.value) && e.target.value !=="") {
+      if (expresiones.precio.test(e.target.value) && e.target.value !== "") {
         price.classList.remove('form-error');
         price.classList.add('form-success');
-        smallprice.innerHTML= ""
+        smallprice.innerHTML = ""
         errors[e.target.id] = false;
       } else {
         price.classList.remove('form-success');
         price.classList.add('form-error');
-        smallprice.innerHTML= "Debe ingresar un PRECIO de 4 a 6 digitos"
+        smallprice.innerHTML = "Debe ingresar un PRECIO de 4 a 6 digitos"
         errors[e.target.id] = true;
       }
       break;
@@ -61,12 +65,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         colors1.classList.remove('form-success');
         colors1.classList.add('form-error');
-        smallcolors.innerHTML= "Debe agregar colores"
+        smallcolors.innerHTML = "Debe agregar colores"
         errors[e.target.id] = true;
       } else {
         colors1.classList.remove('form-error');
         colors1.classList.add('form-success');
-        smallcolors.innerHTML= ""
+        smallcolors.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -74,12 +78,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         colors2.classList.remove('form-success');
         colors2.classList.add('form-error');
-        smallcolors.innerHTML= "Debe agregar colores"
+        smallcolors.innerHTML = "Debe agregar colores"
         errors[e.target.id] = true;
       } else {
         colors2.classList.remove('form-error');
         colors2.classList.add('form-success');
-        smallcolors.innerHTML= ""
+        smallcolors.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -87,12 +91,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         colors3.classList.remove('form-success');
         colors3.classList.add('form-error');
-        smallcolors.innerHTML= "Debe agregar colores"
+        smallcolors.innerHTML = "Debe agregar colores"
         errors[e.target.id] = true;
       } else {
         colors3.classList.remove('form-error');
         colors3.classList.add('form-success');
-        smallcolors.innerHTML= ""
+        smallcolors.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -100,12 +104,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         detalles1.classList.remove('form-success');
         detalles1.classList.add('form-error');
-        smalldetalles.innerHTML= "Destacar 4 detalles del Producto"
+        smalldetalles.innerHTML = "Destacar 4 detalles del Producto"
         errors[e.target.id] = true;
       } else {
         detalles1.classList.remove('form-error');
         detalles1.classList.add('form-success');
-        smalldetalles.innerHTML= ""
+        smalldetalles.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -113,12 +117,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         detalles2.classList.remove('form-success');
         detalles2.classList.add('form-error');
-        smalldetalles.innerHTML= "Destacar 4 detalles del Producto"
+        smalldetalles.innerHTML = "Destacar 4 detalles del Producto"
         errors[e.target.id] = true;
       } else {
         detalles2.classList.remove('form-error');
         detalles2.classList.add('form-success');
-        smalldetalles.innerHTML= ""
+        smalldetalles.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -126,12 +130,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         detalles3.classList.remove('form-success');
         detalles3.classList.add('form-error');
-        smalldetalles.innerHTML= "Destacar 4 detalles del Producto"
+        smalldetalles.innerHTML = "Destacar 4 detalles del Producto"
         errors[e.target.id] = true;
       } else {
         detalles3.classList.remove('form-error');
         detalles3.classList.add('form-success');
-        smalldetalles.innerHTML= ""
+        smalldetalles.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -139,12 +143,12 @@ const validarInput = (e) => {
       if (e.target.value == "") {
         detalles4.classList.remove('form-success');
         detalles4.classList.add('form-error');
-        smalldetalles.innerHTML= "Destacar 4 detalles del Producto"
+        smalldetalles.innerHTML = "Destacar 4 detalles del Producto"
         errors[e.target.id] = true;
       } else {
         detalles4.classList.remove('form-error');
         detalles4.classList.add('form-success');
-        smalldetalles.innerHTML= ""
+        smalldetalles.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -157,7 +161,7 @@ const validarInput = (e) => {
       } else {
         description.classList.remove('form-error');
         description.classList.add('form-success');
-        smalldescription.innerHTML= ""
+        smalldescription.innerHTML = ""
         errors[e.target.id] = false;
       }
       break;
@@ -169,89 +173,105 @@ const validarInput = (e) => {
 
 
 inputs.forEach((input) => {
-  input.addEventListener("keyup", validarInput)
+  input.addEventListener("change", validarInput)
   input.addEventListener("blur", validarInput)
+  input.addEventListener("keyup", validarInput)
 })
-
-
 var btn = document.getElementById("submit-btn");
-btn.onclick = function () {
-  /* ------------ validar select ------------- */
-    const genero = document.getElementById("genero")
-    const temporada = document.getElementById("temporada")
-    const outlet = document.getElementById("outlet")
-    const marca = document.getElementById("marca")
-    if (genero.value == "") {
-      genero.classList.remove('form-success');
-      genero.classList.add('form-error');
-      alert("SELECCIONE UN GENERO!!!")
-    } else {
-      genero.classList.remove('form-error');
-      genero.classList.add('form-success');
-    }
-    if (temporada.value == "") {
-      temporada.classList.remove('form-success');
-      temporada.classList.add('form-error');
-      alert("SELECCIONE SI ES DE TEMPORADA!!!")
-    } else {
-      temporada.classList.remove('form-error');
-      temporada.classList.add('form-success');
-    }
-    if (outlet.value == "") {
-      outlet.classList.remove('form-success');
-      outlet.classList.add('form-error');
-      alert("SELECCIONE SI ES OFERTA!!!")
-    } else {
-      outlet.classList.remove('form-error');
-      outlet.classList.add('form-success');
-    }
-    if (marca.value == "") {
-      marca.classList.remove('form-success');
-      marca.classList.add('form-error');
-      alert("SELECCIONE UNA MARCA!!!")  
-    } else {
-      marca.classList.remove('form-error');
-      marca.classList.add('form-success');
-    
-    }
+btn.onclick = function(){
+/* ------------ validar select ------------- */
+
+const genero = document.getElementById("genero")
+const temporada = document.getElementById("temporada")
+const outlet = document.getElementById("outlet")
+const marca = document.getElementById("marca")
+
+      if (genero.value == "") {
+        genero.classList.remove('form-success');
+        genero.classList.add('form-error');
+        alert("SELECCIONE UN GENERO!!!")
+       
+      } else {
+        genero.classList.remove('form-error');
+        genero.classList.add('form-success');
+       
+      }
+  
+
+      if (temporada.value == "") {
+        temporada.classList.remove('form-success');
+        temporada.classList.add('form-error');
+        alert("SELECCIONE SI ES DE TEMPORADA!!!")
+       
+      } else {
+        temporada.classList.remove('form-error');
+        temporada.classList.add('form-success');
+       
+      }
+ 
+      if (outlet.value == "") {
+        outlet.classList.remove('form-success');
+        outlet.classList.add('form-error');
+        alert("SELECCIONE SI ES OFERTA!!!")
+       
+      } else {
+        outlet.classList.remove('form-error');
+        outlet.classList.add('form-success');
+       
+      }
+     
+      if (marca.value == "") {
+        marca.classList.remove('form-success');
+        marca.classList.add('form-error');
+        alert("SELECCIONE UNA MARCA!!!")
+       
+      } else {
+        marca.classList.remove('form-error');
+        marca.classList.add('form-success');
+       
+      }
+  }
+
+
+
       /*------------  validar checkbox ------------ */
       
         const checkinput = document.querySelectorAll(".tallescheck");
-        let tallesChecked = false;
+  let tallesChecked = false;
 
-        for (let i = 0; i < checkinput.length; i++) {
-            console.log(checkinput[i].checked);
-            if(checkinput[i].checked){
-              tallesChecked = true;
-            }
-                  
-        }
- 
-         if (!tallesChecked) {
-          alert("seleccionar talles")
-          smalltalles.innerHTML= "Seleccionar talles del Producto"
-        } else {
-           smalltalles.innerHTML= ""
-        }
-
-      
-    /* -------------  Validar Imagen -------------*/
-      var imagen = document.getElementById("img").files;
-
-      if (imagen.length == 0) {
-        alert("DEBE SUBIR IMAGENES")
-      } else {
-        for (x = 0; x < imagen.length; x++) {
-          if ((/\.(jpg|jpeg|png|gif|webp)$/).test(imagen.name)) {
-            alert('El archivo no es una imagen'); 
-          }
-        }
-      }
-     if (imagen.size > 320000) {
-        alert('El peso de la imagen no puede exceder los 320kb')
-      }
-      
+  for (let i = 0; i < checkinput.length; i++) {
+    console.log(checkinput[i].checked);
+    if (checkinput[i].checked) {
+      tallesChecked = true;
     }
+
+  }
+
+  if (!tallesChecked) {
+    alert("seleccionar talles")
+    smalltalles.innerHTML = "Seleccionar talles del Producto"
+  } else {
+    smalltalles.innerHTML = ""
+  }
+
+
+  /* -------------  Validar Imagen -------------*/
+  var imagen = document.getElementById("img").files;
+
+  if (imagen.length == 0) {
+    alert("DEBE SUBIR IMAGENES")
+  } else {
+    for (x = 0; x < imagen.length; x++) {
+      if ((/\.(jpg|jpeg|png|gif|webp)$/).test(imagen.name)) {
+        alert('El archivo no es una imagen');
+      }
+    }
+  }
+  if (imagen.size > 320000) {
+    alert('El peso de la imagen no puede exceder los 320kb')
+  }
+
+  /*     } */
 
   submitActivado = () => {
     if (errors.title || errors.price || errors.colors1 || errors.colors2 || errors.colors3 || errors.detalles1 || errors.detalles2 || errors.detalles3 || errors.detalles4 || errors.description) {
@@ -264,4 +284,7 @@ btn.onclick = function () {
   form.addEventListener("submit", (e) => {
     e.preventDefault()
     
+
+
+
   })
